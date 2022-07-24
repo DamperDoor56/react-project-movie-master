23b8fa6700703d5427cb910fdf766d6c67de674b
Ludmi Lopez <ludmit@outlook.com>
2022-07-24 15:11:16 -0300

    restarting-the-app

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-import { useEffect, useState } from "react";
+import logo from './logo.svg';
 import './App.css';
-import './App.scss';
-import SideBar from "./components/SideBar";
-import SearchIcon from './search.svg'
-import MovieCard from "./MovieCard";
-//1e1eeb4
-const API_URL = 'http://www.omdbapi.com?apikey=1e1eeb4';
 
-const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const searchMovies = async(title) =>{
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-
-        setMovies(data.Search)
-    }
-    useEffect(() => {
-        searchMovies('Superman');
-    }, []);
-
-    return (
-        <div className="app">
-             <SideBar />
-            <h1>Fuck react</h1>
-        
-        <div className="search">
-            <input placeholder="Search for movies" 
-            value= {searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}>
-            </input>
-            <img src={SearchIcon} alt='search' 
-            onClick={() => searchMovies(searchTerm)}/>
-        </div>
-
-        {movies?.length > 0
-          ? (
-            <div className="container">
-            {movies.map((movie) => (
-                <MovieCard movie={movie} />
-                ))}
-            </div>
-            ) : (
-            <div className="empty">
-             <h2>No movies found</h2>
-            </div>
-            )}
-
-      
-
-        </div>
-    );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
