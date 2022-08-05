06df0d73a11bcca6cc228936bf05d8090b53b86c
Ludmi Lopez <ludmit@outlook.com>
2022-08-05 16:15:34 -0300

    added-API's

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import Movie from './components/Movie'
+import './index.css';
 
+const featuredApi ="https://api.themoviedb.org/3/movie/550?api_key=523035a60f448d64fb44e36fddc972c8";
+const imageApi = "https://image.tmdb.org/t/p/w1280";
+const searchApi = "https://api.themoviedb.org/3/search/movie?&api_key=523035a60f448d64fb44e36fddc972c8&query=";
 
 function App() {
-  const movies = ['1', '2', '3'];
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(featuredApi).then(res => res.json())
+    .then((data) => {
+      console.log(data);
+      setMovies(data.results);
+
+    });
+    
+  }, []);
+
   return (
     <div>
-      {movies.map(movie =>(
-        <Movie />
-      ))}
+      {movies.length > 0 && movies.map((movie) =><Movie />)}
     </div>
   );
 }
