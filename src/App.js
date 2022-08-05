8f398bb1ae193096490734de67d1cd9419f74545
Ludmi Lopez <ludmit@outlook.com>
2022-08-05 16:50:10 -0300

    fixed-error-length

--- src/App.js
+++ src/App.js
@@ -2,26 +2,25 @@ import React,{ useEffect, useState } from 'react';
 import Movie from './components/Movie'
 import './index.css';
 
-const featuredApi ="https://api.themoviedb.org/3/movie/550?api_key=523035a60f448d64fb44e36fddc972c8";
-const imageApi = "https://image.tmdb.org/t/p/w1280";
+const featuredApi ="https://api.themoviedb.org/3/discover/movie?sorty_by=popularity.desc&api_key=523035a60f448d64fb44e36fddc972c8&page=1";
 const searchApi = "https://api.themoviedb.org/3/search/movie?&api_key=523035a60f448d64fb44e36fddc972c8&query=";
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch(featuredApi).then(res => res.json())
-    .then((data) => {
-      console.log(data);
-      setMovies(data.results);
-
+    fetch(featuredApi)
+      .then((res) => res.json())
+      .then((data) => {
+         console.log(data);
+         setMovies(data.res);
     });
-    
   }, []);
 
   return (
     <div>
-      {movies.length > 0 && movies.map((movie) =><Movie />)}
+      {(movies || []).length > 0 &&
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Movie = () =>{
-    <div className="movie">
-        This is a movie component
-    </div>
-}
+const imageApi = "https://image.tmdb.org/t/p/w1280";
 
+const Movie = ({ title, poster_path, overview, vote_average}) => (
+    <div className="movie">
+        <img src={imageApi + poster_path} alt={title} />
+    </div>
+);
 export default Movie;
