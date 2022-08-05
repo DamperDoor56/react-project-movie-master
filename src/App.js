10028f12ea4fedd587d8a31f783b3cdb55a67e57
Ludmi Lopez <ludmit@outlook.com>
2022-08-05 20:34:56 -0300

    I-made-it-work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React,{ useEffect, useState } from 'react';
 import MovieBox from './components/Movie';
-import Movie from './components/Movie'
 import './index.css';
 
-const API_IMG= 'https://image.tmdb.org/t/p/w500';
 const API_URL= 'https://api.themoviedb.org/3/movie/popular?api_key=523035a60f448d64fb44e36fddc972c8';
-const API_SEARCH = 'https://api.themoviedb.org/3/search/movie?api_key=523035a60f448d64fb44e36fddc972c8&query';
+// const API_SEARCH = 'https://api.themoviedb.org/3/search/movie?api_key=523035a60f448d64fb44e36fddc972c8&query';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const example = ["1", "2"];
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then(data => {
+      console.log(data);
+      setMovies(data.results);
+    })
+  }, [])
 
   return (
     <div>
       <h1>Movies</h1>
-      {example.map((movieReq) => <MovieBox />)}
+      {movies.map((movieReq) =><MovieBox key={movieReq.id} {...movieReq}/>)}
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+const API_IMG= 'https://image.tmdb.org/t/p/w500';
 
-const MovieBox = ({}) => (
+const MovieBox = ({title, poster_path, vote_average, release_date, overview}) => (
     <div>
-        Movie Info
+        <h1>{title}</h1>
+        <img src={API_IMG + poster_path} alt=''></img>
+        <p>{overview}</p>
     </div>
 );
 export default MovieBox;
