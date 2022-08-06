d44787a6c2765ea25bfc1765c87ba8d4080f1c9e
Ludmi Lopez <ludmit@outlook.com>
2022-08-06 12:41:29 -0300

    search-bar-working

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const API_URL= 'https://api.themoviedb.org/3/movie/popular?api_key=523035a60f448
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     fetch(API_URL)
@@ -16,11 +17,37 @@ function App() {
       setMovies(data.results);
     })
   }, [])
+ const searchMovie = async(e) =>{
+    e.preventDefault();
+    console.log("Searching")
+    try{
+      const url= `https://api.themoviedb.org/3/search/movie?api_key=523035a60f448d64fb44e36fddc972c8&query=${query}`;
+      const res = await fetch(url); 
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e){
+      console.log(e)
+    }
+ }
 
+ const changeHandler = (e) =>{
+  setQuery(e.target.value);
+ }
   return (
       <>
       <header>
-        <input className='search' type="text" placeholder="Search"/>
+        <form onSubmit={searchMovie}> 
+        <input
+         className='search'
+         type="text"
+         placeholder="Search"
+         value={query} onChange={changeHandler}
+         name='query'
+         />
+         
+      </form>
       </header>
     <div className='movie-container'>
       {movies.map((movieReq) =><MovieBox key={movieReq.id} {...movieReq}/>)}
