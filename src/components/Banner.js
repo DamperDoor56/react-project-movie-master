0130b700d7fb347afa8788ef2c2dbff6af61a52a
Ludmi Lopez <ludmit@outlook.com>
2022-08-08 10:55:51 -0300

    fixed-axios-error

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,44 +1,23 @@
-import axios from "axios";
+import axios from "./axios.js";
 import React, { useState, useEffect} from "react";
 import requests from "./request";
 
 
 function Banner(){
-    const [movie, setMovie] = useState([]);
+    const [movie, setMovies] = useState([]);
 
-
-        useEffect(() => {
-            async function fetchData() {
-                const request = await axios.get(requests.fetchNetflixOriginals);
-                setMovie(
-                    request.data.results[
-                         Math.floor(Math.random() * (request.data.results.length - 1))
-                        ]
-                );
-               return request;
-            }
-            fetchData();
-        }, []);
-        console.log(movie)
-        
-return (
-    <header className="banner"
-       style={{
-        backgroundSize: 'cover',
-        backgroundPosition: "center center",
-        backgroundImage: `url("https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}")`,
-       }}
-        >
-        <div className="banner__contents">
-            <h1>    
-              {movie?.title || movie?.name || movie?.original_name}</h1>
-              
-              <div className="banner__buttons">
-                <button className="banner__button">Play</button>
-                <button className="banner__button">My List</button>      
-              </div>
-        </div>
-    </header>
-);
+    useEffect (() =>{
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovies(
+                request.data.results[
+                    Math.floor(Math.random() * (request.data.results.length - 1))
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    }, [])
+    console.log(movie)
 }
 export default Banner;
